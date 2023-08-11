ab2daa35a33cb6e8c118159f2342230cbc7e5248
Brittney Epperson <beppers87@hotmail.com>
2023-08-11 12:45:23 -0500

    Finished functionality

--- scripts.js
+++ scripts.js
@@ -1,92 +1,155 @@
-window.addEventListener('load', function() {
-  const add = function(a,b) {
-    return a + b;
-  };
-  
-  const subtract = function(a,b) {
-    return a - b;
-  };
-  
-  const sum = function(arr) {
-    let summedNums = 0;
-    for (let i = 0; i < arr.length; i++) {
-      summedNums += arr[i];
-    }
-    return summedNums;
-  };
-  
-  const multiply = function(...arr) {
-    let multiplyNums = 1;
-    for (let i = 0; i < arr.length; i++) {
-      multiplyNums *= arr[i];
-    }
-    return multiplyNums;
-  };
-  
-  const divide = function(a,b){
-    if (b === 0) {
-      alert("Hey! You can't divide by zero!");
-    }
-    return a / b;
-  
-  };
-  
-  let firstNum;
-  let operator;
-  let secondNum;
-  
-  function operate(firstNum, operator, secondNum) {
-    if (operator === "+") {
-      add(firstNum, secondNum);
-    } else if (operator === "-") {
-      subtract(firstNum, secondNum);
-    } else if (operator === "*") {
-      multiply(firstNum, secondNum);
-    } else if (operator === "/") {
-      divide(firstNum, secondNum);
-    }
-  }
-  
+window.addEventListener("load", function() {
   const calculator = document.querySelector(".calculator");
   const keys = calculator.querySelector(".buttons");
-  
+  const display = document.querySelector(".displayLine");
+
   keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-      const key = e.target;
-      const action = key.dataset.action;
-  
-      if (!action) {
-        console.log('number key!')
-      }
-  
-      if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-      ) {
-        console.log('operator key!')
-      }
+    if (!e.target.matches('button')) return;
+    const key = e.target;
+    const displayedNum = display.textContent;
+    const resultString = createResultString(key, displayedNum, calculator.dataset);
 
-      if (action === 'decimal') {
-        console.log('decimal key!');
-      }
-
-      if (action === 'clear') {
-        console.log('clear key!');
-      }
-
-      if (action === 'calculate') {
-        console.log('equal key!')
-      }
-
-      if (action === 'backspace') {
-        console.log('backspace key!');
-      }
-
-      if (action === 'negative') {
-        console.log('plus minus key!');
-      }
-    }
+    display.textContent = resultString;
+    updateCalculatorState(key, calculator, resultString, displayedNum);
+    updateVisualState(key, calculator);
   });
 });
+
+const calculate = (n1, operator, n2) => {
+  const firstNum = parseFloat(n1);
+  const secondNum = parseFloat(n2);
+  if (operator === 'add') return firstNum + secondNum;
+  if (operator === 'subtract') return firstNum - secondNum;
+  if (operator === 'multiply') return firstNum * secondNum;
+  if (operator === 'divide') return firstNum / secondNum;
+};
+
+const getKeyType = (key) => {
+  const { action } = key.dataset;
+  if (!action) return 'number';
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) return 'operator';
+  return action;
+};
+
+const createResultString = (key, displayedNum, state) => {
+  const keyContent = key.textContent;
+  const keyType = getKeyType(key);
+  const { firstValue,
+         modValue,
+         operator,
+         previousKeyType } = state;      
+
+  if (keyType === 'number') {
+    return displayedNum === '0'|| 
+    previousKeyType === 'operator' || 
+    previousKeyType === 'calculate'
+    ? keyContent
+    : displayedNum + keyContent;
+    }
+
+  if (keyType === 'decimal') {
+    if (!displayedNum.includes('.')) return displayedNum + '.';
+    if (previousKeyType === 'operator' || previousKeyType === 'calculate') return '0.';
+    return displayedNum; 
+  }  
+
+  if (keyType === 'operator') {
+    return firstValue && 
+      operator && 
+      previousKeyType !== 'operator' && 
+      previousKeyType !== 'calculate' 
+      ? calculate(firstValue, operator, displayedNum)
+      : displayedNum;
+  }
+
+  if (keyType === 'clear') return 0;
+
+  if (keyType === 'calculate') {
+    return firstValue     
+  ? previousKeyType === 'calculate'          
+    ? calculate(displayedNum, operator, modValue)
+    : calculate(firstValue, operator, displayedNum)
+  : displayedNum;
+  }
+
+  if (keyType === 'backspace') {
+    displayedNum = displayedNum.slice(0, -1);
+    return displayedNum;
+  }
+
+  if (keyType === 'negative') {
+    displayedNum = displayedNum.slice(0);
+    displayedNum = displayedNum * -1;
+    return displayedNum;
+  }
+};
+
+const updateCalculatorState = (key, calculator, calculatedValue, displayedNum) => {
+  const keyType = getKeyType(key);
+  const { firstValue,
+        modValue,
+        operator,
+        previousKeyType } = calculator.dataset;
+  
+  calculator.dataset.previousKeyType = keyType;
+
+  if (keyType === 'operator') {
+    calculator.dataset.operator = key.dataset.action
+    calculator.dataset.firstValue = firstValue && 
+      operator && 
+      previousKeyType !== 'operator' && 
+      previousKeyType !== 'calculate'
+      ? calculatedValue
+      : displayedNum;
+  }
+
+  if (keyType === 'calculate') {
+    calculator.dataset.modValue = firstValue && previousKeyType === 'calculate'
+        ? modValue
+        : displayedNum;
+  }
+
+  if (keyType === 'clear' && key.textContent === 'AC') {
+        calculator.dataset.firstValue = '';
+        calculator.dataset.operator = '';
+        calculator.dataset.modValue = '';
+        calculator.dataset.previousKeyType = '';
+  }
+};
+
+const updateVisualState = (key, calculator) => {
+  const keyType = getKeyType(key);
+  Array.from(key.parentNode.children).forEach(k =>
+    k.classList.remove('is-depressed')
+  );
+
+  if (keyType === 'operator') key.classList.add('is-depressed');
+
+  if (keyType === 'clear' && key.textContent !== 'AC') {
+    key.textContent = 'AC';
+  }
+
+  if (keyType !== 'clear') {
+    const clearButton = calculator.querySelector('[data-action=clear]');
+    clearButton.textContent = 'CE';
+  }
+};
+
+
+// TODO
+// Limit display line length
+ 
+// function Display(displayedNum) {
+//   document.getElementById("displayLine").innerHTML += displayedNum;
+//   let maximum = 14;
+//   let calc = document.getElementById("displayLine").innerHTML;
+//   if (calc.length > maximum) {
+//     let length = calc.substring(0, 14);
+//     document.getElementById("displayLine").innerHTML = length;
+//   }
+// }
